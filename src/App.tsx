0f9d650cac548de020176a91b6308a0fb3ec79e4
pitric <writt@pitric.com>
2023-07-26 18:38:54 -0600

    bb-2

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 3)}>
-          The count is here... {count}
+          The count is here and it is... {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
